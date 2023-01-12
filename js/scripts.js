4e95fc90cde62b4deb9e4cccd15948dbf377a16b
Jacqueline <jcasalia@yahoo.com.br>
2023-01-12 17:39:02 -0300

    javascript

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const form= document.querySelector("#form")
+// selecionar cada imput sao valores indepedentes
+const nameInput=document.querySelector("#name")
+const emailInput=document.querySelector("#email")
+const passwordInput=document.querySelector("#password")
+const confirmationInput = document.querySelector("confirmation" );
+const jobSelect=document.querySelector("#job")
+const menssageTextarea=document.querySelector("#menssage")
+
+// função validação 
+form.addEventListener("submit",event)
+
