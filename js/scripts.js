81f9e28270df72ee9b68ca9670aaae0636626bae
Jacqueline <jcasalia@yahoo.com.br>
2023-01-17 11:50:23 -0300

    html css e javascript

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,65 @@ const jobSelect=document.querySelector("#job")
 const menssageTextarea=document.querySelector("#menssage")
 
 // função validação 
-form.addEventListener("submit",event)
+form.addEventListener("submit",(event)=>{
+//    validar antes de enviar
+    event.preventDefault();
+
+    // verifica se o nome está vazio o
+    if(nameInput.value===""){
+        alert("Por favor, preencha seu nome");
+        return
+    }
+// se o email está preenchido e se é valido 
+    if(emailInput.value===""||!isEmailValid(emailInput.value))
+    {
+        alert("Por favor, preencha seu email");
+        return
+    }
+// verificar se a senha está preenchida
+if(!validatePassword(passwordInput.value,8)){
+    alert("A senha precisa de no mínio de 8 dígitos");
+    return;
+}
+
+
+// verifica se o plano  foi selecionado
+if(jobSelect.value===""){
+    alert("Por favor, escolha um plano.")
+    return;
+}
+// verifica se a mensagem está preenchida
+if(menssageTextarea.value===""){
+    alert("Por favor escreva uma mensagem.")
+    return;
+}
+
+
+//  se todos os campos estiverem corretamente preenchido envie o formulário (form)
+form.submit();
+});
+
+// função que valida email 
+function isEmailValid(email){
+    // cria uma regex para validar o email
+const emailRegex=new RegExp(
+    // usuario12@host.com.br
+    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/ 
+);
+if(emailRegex.test(email)){
+    return true;
+}
+return false;
+}
+
+// função que valida a senha 
+function validatePassword(password, minDigits){
+    if(password.length>=minDigits){
+        return true
+    }
+    return false
+}
+// function validateConfirmar()
+
+
 
